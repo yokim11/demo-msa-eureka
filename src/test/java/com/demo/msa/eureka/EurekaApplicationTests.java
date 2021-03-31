cb40b5616be47769594edbb00a85d0c1b88c53a9
김영옥 <yokim11@sk.com>
2021-03-31 20:53:13 +0900

    initial project version

---
+++ src/test/java/com/demo/msa/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.msa.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
